cb9ccd25d4e84700331b1cfbef8b597b59f52b18
ldxx <luan8876@163.com>
2019-02-28 13:23:29 +0800

    123

--- src/main/java/com/ldx_first/demo_first/config/dataSourceConfiguration.java
+++ src/main/java/com/ldx_first/demo_first/config/dataSourceConfiguration.java
@@ -1,6 +1,6 @@
 package com.ldx_first.demo_first.config;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -23,14 +23,13 @@ public class dataSourceConfiguration  {
     private String jdbcUsername;
 
     @Bean(name = "dataSource")
-    public ComboPooledDataSource createDataSouce() throws PropertyVetoException{
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass(jdbcDriver);
-        dataSource.setJdbcUrl(jdbcUrl);
-        dataSource.setUser(jdbcUsername);
+    public DruidDataSource createDataSouce() throws PropertyVetoException{
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(jdbcDriver);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(jdbcUsername);
         dataSource.setPassword(jdbcPassword);
         //关闭连接后不自动commit
-        dataSource.setAutoCommitOnClose(false);
         return dataSource;
     }
 
